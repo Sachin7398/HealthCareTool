3ce26959d6eecf1bd9a7642f56ff01c86662ac6f
Sachin <sachinyadav7398129247@gmail.com>
2023-09-29 14:49:23 +0530

    desgine completed

--- index.js
+++ index.js
@@ -13,3 +13,50 @@ hamburger.addEventListener("click", () =>{
     sidebar.style.display = "none";
 
 });
+
+// Get the popup and the button that opens it
+// var popup = document.getElementById("popup");
+// var popupButton = document.getElementById("popupButton");
+// var closeBtn = document.querySelector(".close");
+
+// // Function to open the popup
+// popupButton.onclick = function() {
+//   popup.style.display = "block";
+// };
+
+// // Function to close the popup
+// closeBtn.onclick = function() {
+//   popup.style.display = "none";
+// };
+
+// // Close the popup if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (event.target == popup) {
+//     popup.style.display = "none";
+//   }
+// };
+
+// Get all elements with the class 'popupButton'
+var popupButtons = document.querySelectorAll('.popupButton');
+var popup = document.getElementById('popup');
+var closeBtn = document.querySelector('.close');
+
+// Function to open the popup
+popupButtons.forEach(function(button) {
+    button.onclick = function() {
+        popup.style.display = 'block';
+    };
+});
+
+// Function to close the popup
+closeBtn.onclick = function() {
+    popup.style.display = 'none';
+};
+
+// Close the popup if the user clicks outside of it
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.style.display = 'none';
+    }
+};
+
